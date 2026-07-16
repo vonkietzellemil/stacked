921abdf8d51af0fc3f744653a7e7d30f54a67545
vonkietzellemil <vonkietzellemil@gmail.com>
2026-07-16 17:45:47 +0200

    Add files via upload

--- list-overview/assets/js/settings.js
+++ list-overview/assets/js/settings.js
@@ -1,5 +1,5 @@
 const settingsData = {
-  CURRENT_VERSION: "1.5.1",
+  CURRENT_VERSION: "1.6.0",
   CURRENT_DATA_VERSION: 4,
 };
 
@@ -598,6 +598,7 @@ function checkForUpdates() {
   runDataMigrations();
 
   if (settings.version !== settingsData.CURRENT_VERSION) {
+    console.log("runningnngnn")
     settings.version = settingsData.CURRENT_VERSION;
     if (!settings.receiveUpdateNews) return;
 
